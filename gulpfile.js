8f25fb9e584658e6b97fa6ce7d77b9df861f6c13
AntonioPonte <Antonio.Ponte@mfn-berlin.de>
2016-10-06 13:32:23 +0200

    Creates 4 8 4 layout for desktop.

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,19 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+var livereload = require('gulp-livereload');
 
 gulp.task('sass:prod', function () {
-  gulp.src('./sass/*.scss')
-    .pipe(sass().on('error', sass.logError))
+  //gulp.src('./sass/*.scss')
+  gulp.src('./sass/**/*.scss')
+    //.pipe(sass().on('error', sass.logError))
+    .pipe(sass({
+        includePaths: [
+            './node_modules/breakpoint-sass/stylesheets/',
+            './node_modules/singularitygs/stylesheets/',
+            './node_modules/bourbon/app/assets/stylesheets/'
+        ]
+    }).on('error', sass.logError))
     .pipe(autoprefixer({
        browsers: ['last 2 version']
     }))
@@ -15,18 +24,29 @@ gulp.task('sass:prod', function () {
 });
 
 gulp.task('sass:dev', function () {
-  gulp.src('./sass/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
+  //gulp.src('./sass/*.scss')
+  gulp.src('./sass/**/*.scss')
+  .pipe(sourcemaps.init())
+    //.pipe(sass().on('error', sass.logError))
+    .pipe(sass({
+        includePaths: [
+            './node_modules/breakpoint-sass/stylesheets/',
+            './node_modules/singularitygs/stylesheets/',
+            './node_modules/bourbon/app/assets/stylesheets/'
+        ]
+    }).on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 2 version']
     }))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
+    .pipe(livereload());
 });
 
 gulp.task('sass:watch', function () {
+  livereload.listen();
   gulp.watch('./sass/**/*.scss', ['sass:dev']);
+  gulp.watch('./sass/**/**/*.scss', ['sass:dev']);
 });
 
 gulp.task('default', ['sass:dev', 'sass:watch']);
---
+++ layouts/frontpage/frontpage-layout.tpl.php
@@ -0,0 +1,52 @@
+<div <?php print $attributes; ?>>
+    <header class="l-header" role="banner">
+        <div class="l-branding">
+            <?php if ($logo): ?>
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="site-logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+            <?php endif; ?>
+
+            <?php if ($site_name || $site_slogan): ?>
+                <?php if ($site_name): ?>
+                    <h1 class="site-name">
+                        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+                    </h1>
+                <?php endif; ?>
+
+                <?php if ($site_slogan): ?>
+                    <h2 class="site-slogan"><?php print $site_slogan; ?></h2>
+                <?php endif; ?>
+            <?php endif; ?>
+
+            <?php print render($page['branding']); ?>
+        </div>
+
+        <?php print render($page['header']); ?>
+        <?php print render($page['navigation']); ?>
+    </header>
+
+    <div class="l-main">
+        <?php print render($page['highlighted']); ?>
+        <?php print render($page['featured']); ?>
+        <?php print render($page['slider']); ?>
+        <?php print $messages; ?>
+        <div class="l-content" role="main">
+            <a id="main-content"></a>
+            <?php print render($page['help']); ?>
+            <?php if ($action_links): ?>
+                <ul class="action-links"><?php print render($action_links); ?></ul>
+            <?php endif; ?>
+            <?php print render($page['news']); ?>
+            <?php print render($page['misc']); ?>
+            <?php print $feed_icons; ?>
+        </div>
+    </div>
+    <footer class="l-footer" role="contentinfo">
+        <?php print render($page['footer']); ?>
+        <?php print render($page['footer_first']); ?>
+        <?php print render($page['footer_second']); ?>
+        <?php print render($page['footer_third']); ?>
+        <?php print render($page['footer_fourth']); ?>
+        <?php print render($page['footer_fifth']); ?>
+        <?php print render($page['footer_sixth']); ?>
+    </footer>
+</div>
