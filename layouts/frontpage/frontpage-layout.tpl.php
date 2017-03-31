a7f1c24388bff10d5581df50b07515970e0ce1db
AntonioPonte <Antonio.Ponte@mfn-berlin.de>
2017-03-31 14:49:14 +0200

    update header

--- layouts/frontpage/frontpage-layout.tpl.php
+++ layouts/frontpage/frontpage-layout.tpl.php
@@ -5,17 +5,7 @@
                 <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="site-logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
             <?php endif; ?>
 
-            <?php if ($site_name || $site_slogan): ?>
-                <?php if ($site_name): ?>
-                    <h1 class="site-name">
-                        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-                    </h1>
-                <?php endif; ?>
 
-                <?php if ($site_slogan): ?>
-                    <h2 class="site-slogan"><?php print $site_slogan; ?></h2>
-                <?php endif; ?>
-            <?php endif; ?>
 
             <?php print render($page['branding']); ?>
         </div>
