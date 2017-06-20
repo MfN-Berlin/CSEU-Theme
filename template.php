dc52ceefd1b5c34f15569d91b8b10be915847198
Antonio Ponte <Antonio.Ponte@mfn-berlin.de>
2017-06-20 08:31:47 +0200

    #375: Unterseiten Arbeitsgruppen

--- template.php
+++ template.php
@@ -7,8 +7,9 @@
  */
 
 function cseu_theme_views_pre_render(&$view) {
-  if ($view->name == 'cseu_wgs_list_view') {
-    if ($view->current_display == 'members_page' || $view->current_display == 'wg_page') {
+  if ($view->name == 'cseu_wgs_list_view' || $view->name == 'cseu_wg_resources_list' || $view->name == 'cseu_wg_events_list') {
+    if ($view->current_display == 'members_page' || $view->current_display == 'wg_page'
+        || $view->current_display == 'resources_page' || $view->current_display == 'events_page') {
       if (!empty($view->result[0]->node_title) && !empty($view->result[0]->field_field_cseu_long_label)) {
         $view_title = $view->result[0]->field_field_cseu_long_label[0]['raw']['value'] . ' - ' . $view->result[0]->node_title;
         $view->build_info['title'] = $view_title;
