8987d0a2465b0771b42178d6b61c790c97ee7845
AntonioPonte <Antonio.Ponte@mfn-berlin.de>
2017-11-08 13:47:58 +0100

    #406: PDF Einbindung Website - die Funktion newwindow blank wird nicht angenommen

--- template.php
+++ template.php
@@ -17,3 +17,45 @@ function cseu_theme_views_pre_render(&$view) {
     }
   }
 }
+
+/**
+ * Open PDF file in new window
+ * See: https://www.drupal.org/node/301234
+ */
+function cseu_theme_file_link($variables) {
+  $file = $variables['file'];
+  $icon_directory = drupal_get_path('theme', 'cseu_theme') . '/images/icons';
+  $url = file_create_url($file->uri);
+  $file_size = format_size($file->filesize);
+  $icon = theme('file_icon', array('file' => $file, 'icon_directory' => $icon_directory));
+
+  // Set options as per anchor format described at
+  // http://microformats.org/wiki/file-format-examples
+  $options = array(
+    'attributes' => array(
+      'type' => $file->filemime . '; length=' . $file->filesize,
+    ),
+  );
+
+  // Use the description as the link text if available.
+  if (empty($file->description)) {
+    $link_text = $file->filename;
+  }
+  else {
+    $link_text = $file->description;
+    $options['attributes']['title'] = check_plain($file->filename);
+  }
+
+  // Open files of particular mime types in new window
+  $new_window_mimetypes = array('application/pdf','text/plain');
+  if (in_array($file->filemime, $new_window_mimetypes)) {
+    $options['attributes']['target'] = '_blank';
+  }
+
+  $link_text .= '<br />3445KB';
+  $options['html'] = TRUE;
+
+    return '<div class="file"><div class="cseu-file-icon">' . $icon . '</div>'.
+            '<div class="cseu-file-document"><div class="cseu-file-link">' . l($link_text, $url, $options) .'</div>'.
+            '</div>';
+}
